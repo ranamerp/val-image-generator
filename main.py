913c59f3ee805022064bfcc1cd9705559164425d
Prem Rana <ranamerp@gmail.com>
2024-03-15 18:22:21 -0500

    Updated to use JSON and removed uncessary stuff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import traceback
+import json
 from InquirerPy import inquirer
 from InquirerPy.base.control import Choice
 
@@ -12,18 +13,38 @@ def main():
 
     fetch_images('agent')
     fetch_images('map')
-    choices = fetch_matches(mgr, content) + [Choice(name="Reload", value="reload"), Choice(name="Exit", value="exit")]
+    #choices = fetch_matches(mgr, content) + [Choice(name="Reload", value="reload"), Choice(name="Exit", value="exit")]
+    new_choices = [Choice(name="Use JSON", value="json"), Choice(name="Enter Values", value="values"), Choice(name="Exit", value="exit")]
     previous_choices = {
         "team_a": "DEF",
         "team_b": "ATK",
         "primary": "#b6a46d",
         "secondary": "#000000",
         "tertiary": "#ffffff",
-        "logo": "data/misc_assets/logo.png" 
+        "logo": "data/misc_assets/logo.png",
+        "output": True
     }
     while True:
-        match_id = inquirer.select("Pick a match:", choices).execute()
-        if match_id == "custom":
+        match_id = inquirer.select("Pick an option:", new_choices).execute()
+        if match_id == "json":
+            match_id = inquirer.text("Enter match id (Found on tracker.gg)").execute()
+            overwrite = inquirer.select("Overwrite Team Names from File?:", [Choice(name="Yes", value=True), Choice(name="No", value=False)]).execute()
+            with open("colors.json", "r") as data:
+                choices = json.load(data)
+                team_a = choices['team_a']
+                team_b = choices['team_b']
+                primary_color = choices['primary']
+                secondary_color = choices['secondary']
+                tertiary_color = choices['tertiary']
+                logo_path = choices['logo']
+                path_var = bool(choices['output'])
+            
+            if overwrite:
+                team_a = inquirer.text("Enter Team A (Attackers, Left Side of Lobby)", default=previous_choices['team_a']).execute()
+                team_b = inquirer.text("Enter Team B (Defenders, Right Side of Lobby)", default=previous_choices['team_b']).execute()
+
+
+        elif match_id == "custom":
             match_id = inquirer.text("Enter match id (Found on tracker.gg)").execute()
             team_a = inquirer.text("Enter Team A (Attackers)", default=previous_choices['team_a']).execute()
             previous_choices['team_a'] = team_a
@@ -51,9 +72,9 @@ def main():
             
         elif match_id == "exit":
             break
-        elif match_id == "reload":
-            choices[:-2] = fetch_matches(mgr, content)
-            continue
+        # elif match_id == "reload":
+        #     choices[:-2] = fetch_matches(mgr, content)
+        #     continue
         if match_id is not None and match_id != "":
             try:
                 print("Generating image...")
